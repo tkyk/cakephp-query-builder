f063c3847aeecd6214d41a3a47aa30e1c7b70039
Takayuki Miwa <i@tkyk.name>
2012-02-23 15:58:43 +0900

    Confirming Cake coding standards

--- Model/Behavior/QueryBuilderBehavior.php
+++ Model/Behavior/QueryBuilderBehavior.php
@@ -40,118 +40,118 @@ App::import('Lib', 'QueryBuilder.QueryOptions');
  */
 class QueryBuilderBehavior extends ModelBehavior {
 
-    /**
-     * @var array
-     */
-    public $errorTemplate = array('code' => 500,
-                                  'name' => 'Missing Query Options');
+/**
+ * @var array
+ */
+	public $errorTemplate = array('code' => 500,
+		'name' => 'Missing Query Options');
 
-    /**
-     * This method is public but not available through the Models.
-     * 
-     * @param string  config name
-     */
-    public function _missingOptionsError($name) {
-        return $this->errorTemplate + array('message' => $name);
-    }
+/**
+ * This method is public but not available through the Models.
+ * 
+ * @param string  config name
+ */
+	public function _missingOptionsError($name) {
+		return $this->errorTemplate + array('message' => $name);
+	}
 
-    /**
-     * Returns a query options array named $name
-     * 
-     * @param string  option name
-     * @return array
-     */
-    public function getQueryOptions($model, $name) {
-        if(!isset($model->queryOptions) ||
-           !isset($model->queryOptions[$name]) ||
-           !is_array($model->queryOptions[$name])) {
-            $model->cakeError('error',
-                              $this->_missingOptionsError($name));
-            return;
-        }
-        return $model->queryOptions[$name];
-    }
+/**
+ * Returns a query options array named $name
+ * 
+ * @param string  option name
+ * @return array
+ */
+	public function getQueryOptions($model, $name) {
+		if (!isset($model->queryOptions) ||
+			!isset($model->queryOptions[$name]) ||
+			!is_array($model->queryOptions[$name])) {
+				$model->cakeError('error',
+					$this->_missingOptionsError($name));
+				return;
+			}
+		return $model->queryOptions[$name];
+	}
 
-    /**
-     * An 'Objectified' version of find method.
-     * 
-     * <code>
-     * $finder = $Model->finder('all')->fields(...)->conditions(...);
-     * 
-     * // using query options
-     * $finder = $Model->finder('all', 'approved', 'normal_order')->limit(...);
-     * </code>
-     * 
-     * @param object  Model
-     * @param string  find type
-     * @return QueryMethod
-     */
-    public function finder($model, $type) {
-        $finder = $model->createQueryMethod('find', array($type));
-        if(func_num_args() > 2) {
-            $args = func_get_args();
-            $finder->import(array_slice($args, 2));
-        }
-        return $finder;
-    }
+/**
+ * An 'Objectified' version of find method.
+ * 
+ * <code>
+ * $finder = $Model->finder('all')->fields(...)->conditions(...);
+ * 
+ * // using query options
+ * $finder = $Model->finder('all', 'approved', 'normal_order')->limit(...);
+ * </code>
+ * 
+ * @param object  Model
+ * @param string  find type
+ * @return QueryMethod
+ */
+	public function finder($model, $type) {
+		$finder = $model->createQueryMethod('find', array($type));
+		if (func_num_args() > 2) {
+			$args = func_get_args();
+			$finder->import(array_slice($args, 2));
+		}
+		return $finder;
+	}
 
-    /**
-     * Creates a new QueryMethod object bound to the $model.
-     * 
-     * @param object  Model
-     * @param string  method name
-     * @param array   arguments
-     * @return QueryMethod
-     */
-    public function createQueryMethod($model, $method, $args=array()) {
-        return new QueryMethod($model, $method, $args);
-    }
+/**
+ * Creates a new QueryMethod object bound to the $model.
+ * 
+ * @param object  Model
+ * @param string  method name
+ * @param array   arguments
+ * @return QueryMethod
+ */
+	public function createQueryMethod($model, $method, $args=array()) {
+		return new QueryMethod($model, $method, $args);
+	}
 
-    /**
-     * Wrapper method for the Controller->paginate method.
-     * 
-     * @param object  Controller
-     * @param array   find-compatible options array
-     * @return mixed
-     */
-    public function execPaginate($model, $controller, $options=array()) {
-        $controller->paginate[$model->alias] = $options;
-        return $controller->paginate($model->alias);
-    }
+/**
+ * Wrapper method for the Controller->paginate method.
+ * 
+ * @param object  Controller
+ * @param array   find-compatible options array
+ * @return mixed
+ */
+	public function execPaginate($model, $controller, $options=array()) {
+		$controller->paginate[$model->alias] = $options;
+		return $controller->paginate($model->alias);
+	}
 
-    /**
-     * An 'objectified' version of execPaginate method.
-     * 
-     * @param object  Model
-     * @param object  Controller
-     * @return QueryMethod
-     */
-    public function paginator($model, $controller) {
-        $paginator = $model->createQueryMethod('execPaginate', array($controller));
-        if(func_num_args() > 2) {
-            $args = func_get_args();
-            $paginator->import(array_slice($args, 2));
-        }
-        return $paginator;
-    }
+/**
+ * An 'objectified' version of execPaginate method.
+ * 
+ * @param object  Model
+ * @param object  Controller
+ * @return QueryMethod
+ */
+	public function paginator($model, $controller) {
+		$paginator = $model->createQueryMethod('execPaginate', array($controller));
+		if (func_num_args() > 2) {
+			$args = func_get_args();
+			$paginator->import(array_slice($args, 2));
+		}
+		return $paginator;
+	}
 
-    /**
-     * Creates a new SubqueryExpression object.
-     * 
-     * @param object  Model
-     * @param string  [optional] table name or alias
-     * @param string  [optional] table name or alias
-     * @return object  SubqueryExpression
-     */
-    public function subquery($model, $tableOrAlias=null, $tableOrAlias2=null) {
-        $exp = new SubqueryExpression($model);
-        if(is_string($tableOrAlias)) {
-            $exp->tableOrAlias($tableOrAlias);
-        }
-        if(is_string($tableOrAlias2)) {
-            $exp->tableOrAlias($tableOrAlias2);
-        }
-        return $exp;
-    }
+/**
+ * Creates a new SubqueryExpression object.
+ * 
+ * @param object  Model
+ * @param string  [optional] table name or alias
+ * @param string  [optional] table name or alias
+ * @return object  SubqueryExpression
+ */
+	public function subquery($model, $tableOrAlias=null, $tableOrAlias2=null) {
+		$exp = new SubqueryExpression($model);
+		if (is_string($tableOrAlias)) {
+			$exp->tableOrAlias($tableOrAlias);
+		}
+		if (is_string($tableOrAlias2)) {
+			$exp->tableOrAlias($tableOrAlias2);
+		}
+		return $exp;
+	}
 }
 
